83846621890eb62959bd77a01b3d182f4611a767
Sakthiveldeveloper <sakthivel24498@gmail.com>
2023-01-04 14:34:55 +0530

    30s session origin

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges : false,  //help to not automaticaly run the test after changes save only run when we restart the test
     specPattern : 'cypress/e2e/*/*.cy.{js,jsx,ts,tsx}', //help to write all  file in Cypress
-    specPattern : 'cypress/e2e/*/*.{js,jsx,ts,tsx}',
+    specPattern : 'cypress/e2e/*/*.{js,jsx,ts,tsx,feature}',
+    // chromeWebSecurity : false, //web sequrity  disable in the chrome only
+    // experimentalSessionAndOrigin  :true //help to run origin but v12 remove this
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
